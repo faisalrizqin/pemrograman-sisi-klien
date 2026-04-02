17ceeea9463e0aeb1e0febfad00a506ae24c10c1
faisalrizqin <faisalrizqitwenty7@gmail.com>
2026-04-02 15:50:00 +0700

    commit project

---
+++ es6.js
@@ -0,0 +1,12 @@
+const mahasiswa = {
+    nim: "22001",
+    nama: "Budi Santoso",
+    status: true,
+    matkul: [{ matkulId: "MK001", tugas: 85, uts: 88, uas: 90 }]
+};
+
+const mahasiswaList = [
+    { nim: "22001", nama: "Budi Santoso", status: true },
+    { nim: "22002", nama: "Siti Aminah", status: false }
+];
+
